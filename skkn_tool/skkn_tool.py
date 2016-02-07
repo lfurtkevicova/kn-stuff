9fff406895683616555f74d7729c45c23c855360
Ludka Furtkevicova <ludmilafurtkevicov@gmail.com>
2016-02-07 12:59:51 +0100

    subprocess, writing to gui dialog + some changes in structure

--- skkn_tool/skkn_tool.py
+++ skkn_tool/skkn_tool.py
@@ -20,7 +20,7 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
+from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, QProcess
 from PyQt4.QtGui import QAction, QIcon, QFileDialog, QMessageBox
 # Initialize Qt resources from file resources.py
 import resources
@@ -28,6 +28,7 @@ import resources
 from skkn_tool_dialog import skkn_toolDialog
 import os.path
 from db_manager.db_plugins import createDbPlugin
+from db_manager.db_plugins.plugin import DbError
 import subprocess
 
 
@@ -58,6 +59,7 @@ class skkn_tool:
         # Declare instance attributes
         self.actions = []
         self.menu = self.tr(u'&SKKN tool')
+        
         # TODO: We are going to let the user set this up in a future iteration
         self.toolbar = self.iface.addToolBar(u'skkn_tool')
         self.toolbar.setObjectName(u'skkn_tool')
@@ -72,8 +74,18 @@ class skkn_tool:
         # conversation
         self.dlg.buttonConvert.clicked.connect(self.convertData)
         
+        # ukoncenie konverzie
+        self.dlg.buttonKill.clicked.connect(self.stopConvert)
+        
         # about message
         self.dlg.buttonAbout.clicked.connect(self.showError)
+        
+        # nieco ako Popen, nativne pre Qt
+        self.process = QProcess(self.dlg)
+        self.process.readyRead.connect(self.writeData)
+        
+        # vymazanie schemy
+        self.dlg.buttonDelete.clicked.connect(self.db_deleteSchema)
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -138,31 +150,67 @@ class skkn_tool:
         del self.toolbar
 
     def select_data(self):
-        self.foldername = QFileDialog.getExistingDirectory(self.dlg, "Select data folder (*.vgi data, *.dbf data, outputs)","/home/ludka/Desktop/data")
+        self.foldername = QFileDialog.getExistingDirectory(self.dlg, "Select data folder (*.vgi data, *.dbf data, outputs)","/home/ludka/Desktop")
         self.dlg.lineData.setText(self.foldername)
 
     def convertData(self):
         #self.dlg.textEditData.setText(self.foldername)
-
-        bashCommand = "source convert.sh"
-        os.system(bashCommand)
+    
         
-        #subprocess.call('source convert.sh')
+        # spustenie pomocou call a Popen
+        
+        # subprocess.call([os.path.join(self.plugin_dir,'kataster-import','kt-sql'),self.foldername])
+        # subprocess.Popen([os.path.join(self.plugin_dir,'kataster-import','kt-sql'),self.foldername],shell = True)
+        
+        self.process.start(os.path.join(self.plugin_dir,'kataster-import','kt-sql'),[self.foldername])
+
+    #funkcia na zapisovanie do GUI okna, vola funkciu insertText    
+    def writeData(self):
+        self.insertText(str(self.process.readAll()))
+             
+    def insertText(self,text):
+        cursor = self.dlg.textEditData.textCursor()
+        cursor.movePosition(cursor.End)
+        cursor.insertText(text)
+        self.dlg.textEditData.ensureCursorVisible()
+
+    def stopConvert(self):
+        self.process.kill()
+        self.insertText('Conversation process killed!')
 
     def db_changed(self,index): 
         self.dlg.comboBox_3.clear()
         self.dlg.comboBox_3.addItems(self.db_getSchema(index))
 
+    # naplnenie comboboxu schemami
     def db_getSchema(self,index):
         schemas = []        
-        c = self.dbconnections[index]        
-        c.connect()     
-        for schema in c.database().schemas():
-                schemas.append(schema.name)
+        self.dbconn = self.dbconnections[index]        
+        self.dbconn.connect() 
+        
+        # zabraniene zobrazeniu schem public a topology
+        for schema in self.dbconn.database().schemas():
+            if schema.name in ('public','topology'):
+                continue
+            schemas.append(schema.name)
         return schemas
+    
+    # vymazanie schemy    
+    def db_deleteSchema(self):
+        index = self.dlg.comboBox_3.currentIndex()   
+        schema = self.dlg.comboBox_3.currentText()  
+        db = self.dbconn.database()
 
+        # vlozenie chybovej hlasky v pripade problemu
+        try:    
+            db.sqlResultModel('DROP schema {0} CASCADE'.format(schema), db)
+        except DbError as e:
+            self.insertText(str(e))
+        # vymazanie z comboboxu
+        self.dlg.comboBox_3.removeItem(index)
+        
     def showError(self):
-        mes = QMessageBox.about(None,"About SKKN Plugin","This tool helps users to use Slovak land \
+        QMessageBox.about(None,"About SKKN Plugin","This tool helps users to use Slovak land \
 registry data (cadastral data) in exchange formats created by The Geodesy, Cartography and \
 Cadastre Authority of Slovak republic, in QGIS. \nIt is only usefull and dedicated for processing \
 in Slovak republic for people having access to Cadastre data. \nThere is no reason to use it for \
