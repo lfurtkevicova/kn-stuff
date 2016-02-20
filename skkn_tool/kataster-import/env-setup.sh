febc11dadd9a988b7ef84ecbe783b1097c2f1fee
Ludka Furtkevicova <ludmilafurtkevicov@gmail.com>
2016-02-20 19:35:52 +0100

    kataster-import deleted

--- skkn_tool/kataster-import/env-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Set environment to allow running without installation.
-# Usage: source ./env-setup.sh
-
-export PYTHONPATH=$(readlink -e $(pwd))
-export PATH=$PATH:$(readlink -e $(pwd))
-
-# vim: set ts=4 sts=4 sw=4 noet:
